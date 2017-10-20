d3396f69324bbb440b80e60482aaadf438b9ed4e
Paul Fontaine <ipaulfontaine@gmail.com>
2017-10-19 19:45:29 -0700

    again

--- app.js
+++ app.js
@@ -26,10 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/fa17g01', index);
 app.use('/', index);
-
-app.use('/fa17g01/about', index);
-app.use('/about', about);
-
+//app.use('/about', about);
 app.use('/search', search);
 
 // catch 404 and forward to error handler
