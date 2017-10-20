05acfc296eabb64f85db3d440e2fa1b5ab6cfa1f
Paul Fontaine <ipaulfontaine@gmail.com>
2017-10-19 19:22:06 -0700

    might be better

--- app.js
+++ app.js
@@ -24,10 +24,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use('', index);
 app.use('/', index);
-app.use('/about/', about);
-app.use('/search/', search);
+app.use('/about', about);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
