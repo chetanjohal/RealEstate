8c8f791851ac4d676dc57f0140548490eb86a424
Paul Fontaine <ipaulfontaine@gmail.com>
2017-10-19 19:52:04 -0700

    last one for today

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-    res.render('about');
-});
-/* GET about pages. */
-router.get('/about/Joseph', function(req, res, next) {
-	res.render('abouts/aboutJoseph');
-});
-router.get('/about/Sannuj', function(req, res, next) {
-	res.render('abouts/aboutSannuj');
-});
-
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -5,24 +5,24 @@ router.get('/', function(req, res, next) {
     var word = req.query.word;
     //console.log(word);
     //res.send(word);
-    var mysql      = require('mysql');
-    var connection = mysql.createConnection({
-      host     : 'localhost',
-      user     : 'fa17g01',
-      password : 'csc648fa17g01',
-      database : 'fa17g01'
-    });
-    connection.connect();
-
-    connection.query("SELECT * FROM houses;");
-    connection.query
-        .on('error', function(err) {
-            console.log( err );
-        })
-        .on('result', function( data ) {
-            socket.emit('YourData',data);
-        });
-    connection.end();
+    // var mysql      = require('mysql');
+    // var connection = mysql.createConnection({
+    //   host     : 'localhost',
+    //   user     : 'fa17g01',
+    //   password : 'csc648fa17g01',
+    //   database : 'fa17g01'
+    // });
+    // connection.connect();
+    //
+    // connection.query("SELECT * FROM houses;");
+    // connection.query
+    //     .on('error', function(err) {
+    //         console.log( err );
+    //     })
+    //     .on('result', function( data ) {
+    //         socket.emit('YourData',data);
+    //     });
+    // connection.end();
     res.render('results', { word: word });
 });
 
