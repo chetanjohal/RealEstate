0885805025ce61d0ef1dfd168a6c7e966adf681d
Paul Fontaine <ipaulfontaine@gmail.com>
2017-10-19 18:03:57 -0700

    try connecting to db

--- routes/search.js
+++ routes/search.js
@@ -2,9 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 router.get('', function(req, res, next) {
-    var word = req.param('word');
+    var word = req.query.word;
     //console.log(word);
     //res.send(word);
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'fa17g01',
+      password : 'csc648fa17g01',
+      database : 'fa17g01'
+    });
+    connection.connect();
+
+    connection.query("SELECT * FROM houses;");
+    connection.query
+        .on('error', function(err) {
+            console.log( err );
+        })
+        .on('result', function( data ) {
+            socket.emit('YourData',data);
+        });
+    connection.end();
     res.render('results', { word: word });
 });
 
