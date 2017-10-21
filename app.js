d4f620920b7d91dc210a59728df32853aa836077
Paul Fontaine <ipaulfontaine@gmail.com>
2017-10-20 17:05:37 -0700

    again

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
+var all = require('./routes/all');
 var index = require('./routes/index');
 var about = require('./routes/about');
 var search = require('./routes/search');
@@ -26,10 +27,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/static', express.static(path.join(__dirname, 'public')))
 
-//app.use('/fa17g01', index);
-app.use('/', index);
-app.use('/about/', about);
-app.use('/search', search);
+app.use('/', all);
+//app.use('/fa17g01', all);
+// app.use('/about', about);
+// app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
