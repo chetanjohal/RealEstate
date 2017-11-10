5eea28251d69ea4e06305e3265cd1b6c841c0964
Paul Fontaine <ipaulfontaine@gmail.com>
2017-11-09 16:21:10 -0800

    api defined

--- routes/middleware.js
+++ routes/middleware.js
@@ -4,10 +4,18 @@ var router = express.Router();
 var about = require('./about');
 var search = require('./search');
 var index = require('./index');
+var house = require('./house');
+var client = require('./client');
+var agent = require('./agent');
+
 
 router.use('/', index);
 router.use('/about', about);
 router.use('/search', search);
+router.use('/house', house);
+router.use('/client', client);
+router.use('/agent', agent);
+
 
 router.get('/fa17g01', function(req, res, next) {
   res.redirect('/fa17g01/');
